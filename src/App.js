22e920f41b98a64ae068e86edb5c059bacbb6ee3
Divye Gupta <divyegupta00@gmail.com>
2022-05-22 12:23:46 +0530

    add: links and routes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 // import Login from './Login';
 // import Authentication from './Authentication';
-import Navbar from './Navbar';
-import Home from './Home';
-import Upperbar from './Upperbar';
-import Landingpage from './Landingpage';
-// import Like from './Like';
-// import Chat from './Chat';
-// import Profile from './Profile';
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Upperbar from "./Upperbar";
+// import Landingpage from "./Landingpage";
+import Like from './Like';
+import Chat from './Chat';
+import Profile from './Profile';
 
-// import { Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
+
+
+
   return (
-    <>
-    <div className="App">
-      {/* <Authentication/> */}
-      {/* <Login/> */}
-      {/* <Upperbar/> */}
-      {/* <Home/> */}
-      <Landingpage/>
-    </div>
-    {/* <Router>
-    <Switch>
+    <Router>
+      <div className="App">
+        {/* <Authentication/> */}
+        {/* <Login/> */}
+        <Upperbar/>
+        {/* <Home/> */}
+        {/* <Landingpage /> */}
+        <Switch>
         <Route exact path="/" component ={Home} ></Route>
         <Route exact path="/like" component={Like}></Route>
         <Route exact path="/chat" component={Chat}></Route>
         <Route exact path="/profile" component={Profile}></Route>
       </Switch>
-    </Router> */}
-    <div>
-        {/* <Navbar/> */}
+        <Navbar/>
       </div>
-    </>
+    </Router>
   );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./css/Navbar.css";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
@@ -9,14 +9,14 @@ import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
 function Navbar() {
   return (
     <div className="navbar">
-      {/* <Link to = "/"><HomeOutlinedIcon fontSize="large"/></Link> 
+      <Link to = "/"><HomeOutlinedIcon fontSize="large"/></Link> 
       <Link to = "/chat"><ChatOutlinedIcon fontSize="large"/></Link> 
       <Link to = "/like"><FavoriteBorderIcon fontSize="large"/></Link> 
-      <Link to = "/profile"><PersonOutlinedIcon fontSize="large"/></Link>  */}
-      <HomeOutlinedIcon fontSize="large"/>
+      <Link to = "/profile"><PersonOutlinedIcon fontSize="large"/></Link> 
+      {/* <HomeOutlinedIcon fontSize="large"/>
       <ChatOutlinedIcon fontSize="large"/>
       <FavoriteBorderIcon fontSize="large"/>
-      <PersonOutlinedIcon fontSize="large"/>
+      <PersonOutlinedIcon fontSize="large"/> */}
 
     </div>
   );
